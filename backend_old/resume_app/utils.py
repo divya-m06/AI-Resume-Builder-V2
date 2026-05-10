4038d79ae4c668a61d54e7b62832418f536f0683
divya-m06 <divyamandi3@gmail.com>
2026-05-10 12:18:01 +0530

    hi

--- backend_old/resume_app/utils.py
+++
@@ -1,9 +0,0 @@
-from docx import Document
-
-def extract_docx(file_path):
-    """Extract text from a DOCX file."""
-    doc = Document(file_path)
-    text = []
-    for para in doc.paragraphs:
-        text.append(para.text)
-    return '\n'.join(text)
